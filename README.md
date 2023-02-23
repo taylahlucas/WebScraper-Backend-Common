fb8a033f7904963edc6e5c2fcaf8b708f40f352b
Taylah Lucas <Taylah.lucas@hunterindustries.com>
2023-02-23 12:27:34 +0000

    Merge WebScraper common

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # WebScraper-Backend-Common
 Common backend for building web scraping programs.
+
+### Packages
+    - pip install requests
+    - pip install beautifulsoup4
+    - pip install scrapy-useragents
+    - pip install fake-useragent
+    - pip install user-agents
+    - pip install pymongo
+    - npm install express mongodb body-parser --save
+
+
+### Run Application
+node app.js
--- scrapers/scrapers/get_proxies.py
+++ scrapers/scrapers/get_proxies.py
@@ -6,7 +6,7 @@ from spiders.proxy_spider import ProxySpider
 from scrapy.crawler import CrawlerProcess
 from user_agents import get_ua
 
-# Runs the proxy spider and generates a list of usable proxies
+# Runs the proxy spider and find a usable proxy
 def crawl_proxy_spider():
     process = CrawlerProcess(settings={
         'USER_AGENT': get_ua(),
@@ -36,13 +36,14 @@ def get_proxies():
                 test_url,
                 proxies={"http": ip, "https": ip}
             )
-            print("Successful Connection: ", response.json())
+            print("Successful Connection: ", response)
             successful_proxies.append(ip)
             break
         except requests.exceptions.ProxyError:
             print("Skipping. Connection error")
 
     if len(successful_proxies) > 0:
+        print("Writing proxy to file")
         with open('valid_proxy.json', 'w') as file:
             file.write(json.dumps(successful_proxies[0]))
             file.close()
