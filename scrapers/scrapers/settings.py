4d91bbb62517d62895f10b2a1b6735bbc18618dc
Taylah Lucas <Taylah.lucas@hunterindustries.com>
2023-02-23 13:00:29 +0000

    update

--- scrapers/scrapers/settings.py
+++ scrapers/scrapers/settings.py
@@ -85,8 +85,8 @@ AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 86400
-HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 86400
+# HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
